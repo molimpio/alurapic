3b6162f43d57a1036b83e3c9b0c600a33004f94e
Moises Olimpio <molimpio@hotmail.com.br>
2016-07-02 10:43:47 -0300

    aula 3 ng-repeat, dados do servidor

---
+++ public/js/controllers/fotos-controller.js
@@ -0,0 +1,21 @@
+angular.module('alurapic').controller('FotosController', function ($scope, $http) {
+
+    $scope.fotos = [];
+
+    //forma resumida de acessar a url
+    $http.get('v1/fotos')
+        .success(function (fotos) {
+            $scope.fotos = fotos;
+        })
+        .error(function (erro) {
+            console.warn(erro);
+        });
+
+    /* exemplo com promisse
+     $http.get('v1/fotos');
+     promise.then(function (retorno) {
+     $scope.fotos = retorno.data;
+     }).catch(function(error){
+     console.log("erro",error);
+     });*/
+});
